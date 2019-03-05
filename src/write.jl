673682e7832d81656082360b475e37252639cc8f
김용희 <yongheekim@devsisters.com>
2019-03-05 19:15:29 +0900

    remove Vector{Dict} data, if value is null

--- src/write.jl
+++ src/write.jl
@@ -9,7 +9,12 @@ function JSON.show_json(io::SC, s::CS, jws::JSONWorksheet)
 
         for el in pairs(row)
             if isa(el[2], Array{T} where T <: AbstractDict)
-                JSON.show_pair(io, s, el)
+                tmp = el[2]
+                for (i, x) in enumerate(tmp)
+                    x2 = filter(x -> !ismissing(x[2]), x)
+                    tmp[i] = filter(x -> length(x) > 0, x2)
+                end
+                JSON.show_pair(io, s, Pair(el[1], tmp))
             elseif isa(el[2], Array{T} where T)
                 JSON.show_key(io, el[1])
                 JSON.indent(io)
