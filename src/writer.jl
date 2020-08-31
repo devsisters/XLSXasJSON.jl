a703c879d36a07698e11ea3396dc0d8dd61cef8b
YongheeKim <masterplanf@gmail.com>
2020-08-31 23:48:08 +0900

    Revert "remove drop_null"
    This reverts commit 9254bb225f149afc54a82a29ec79154c8c66e19f.

--- src/writer.jl
+++ src/writer.jl
@@ -1,6 +1,20 @@
 JSON.json(jws::JSONWorksheet) = JSON.json(data(jws))
 JSON.json(jws::JSONWorksheet, indent) = JSON.json(data(jws), indent)
 
+function dropnull(s)
+    replace(s, r"(\"[\w]*\":null,)|(,?\"[\w]*\":null)" => "")
+end
+
+function write(file::Union{String, IO}, jws::JSONWorksheet; indent = 2, drop_null = false)
+    open(file, "w") do io
+        data = JSON.json(jws, indent)
+        if drop_null
+            data = dropnull(data)
+        end
+
+        Base.write(io, data)
+    end
+end
 
 function write(path::String, jwb::JSONWorkbook; kwargs...)
     f = splitext(basename(xlsxpath(jwb)))[1]
